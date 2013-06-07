838c5c57c03a46786aed45a204c851ab130bd5d1
John Brunton <john_brunton@hotmail.co.uk>
2013-06-07 11:21:40 +0100

    more complex test case to model composition like the flex book page

--- Resources/app.js
+++ Resources/app.js
@@ -2,37 +2,96 @@ var win = Titanium.UI.createWindow({
         backgroundColor : '#fff'
 });
 
+function createImageViewFromComposite(compositeView, left, top, width, height) {
+    var tmpImage = compositeView.toImage();
+    tmpImage.backgroundColor = "transparent";
+
+    var imageView = Titanium.UI.createImageView({
+        width: width,
+        height: height,
+        image: tmpImage,
+        top: top,
+        backgroundColor: "transparent"
+    });
+
+    return imageView;    
+}
+
+function createGridPage() {
+    var compositeView = Ti.UI.createView({
+           width: "100dp",
+           height: "17dp",
+           backgroundColor: "#fff"
+    });
+    
+    for (var i = 0; i < 10; ++i) {
+        var cell = Ti.UI.createView({
+                width : "10dp",
+                height : "17dp",
+                left: "" + (i * 10) + "dp",
+                zIndex: 10,
+                backgroundColor: i % 2 == 0 ? "#fff" : "#ccf"
+        });
+    
+        compositeView.add(cell);        
+    }
+    
+    var imageView = createImageViewFromComposite(compositeView, 0, "20dp", "100dp", "17dp");
+    
+    return imageView;
+}
+
+function createGridHeader() {
+    var compositeView = Ti.UI.createView({
+           width: "100dp",
+           height: "17dp",
+           backgroundColor: "#fff"
+    });
+    
+    var label = Ti.UI.createLabel({
+        text: "testing..",
+        top: 0,
+        verticalAlign: 'top',
+        font: {fontSize: '9dp'},
+        color: '#4d4d4f',
+        height: 20
+    });
+    
+    compositeView.add(label);
+    
+    var imageView = createImageViewFromComposite(compositeView, 0, 0, "100dp", "17dp");
+    
+    return imageView;
+}
+
 function createGrid() {
     var compositeView = Ti.UI.createView({
            width: "288dp",
-           height: "17dp",
-           backgroundColor: "#fcc"
+           height: "37dp",
+           backgroundColor: "#eee"
     });
     
-    var grid = Ti.UI.createImageView({
+    var gridPage = createGridPage();
+    compositeView.add(gridPage);
+    
+    var gridHeader = createGridHeader();
+    compositeView.add(gridHeader);
+
+    var grid = Ti.UI.createView({
             width : "288dp",
             height : "17dp",
-            backgroundColor: "transparent",
-            image : "availability_shell.png",
+            top: "20dp",
+            backgroundImage: "availability_shell.png",
             zIndex: 15
     });
     
     compositeView.add(grid);
-     
-    var tmpImage = compositeView.toImage();
-    tmpImage.backgroundColor = "transparent";
     
-    var imageView = Titanium.UI.createImageView({
-        width: "288dp",
-        height: "17dp",
-        image: tmpImage,
-        top: "10dp",
-        backgroundColor: "transparent"
-    });
-    
+    var imageView = createImageViewFromComposite(compositeView, 0, "20dp", "288dp", "37dp");
+
     return imageView;    
 }
 
 win.add(createGrid());
- 
+
 win.open();
